26f87e42e5e57819a675f676bca8428b9e0f51a3
Yumin Chen <yumin@chen.so>
2023-07-08 12:55:34 +0100

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,25 @@
 import { defineConfig as definePandaConfig } from '@pandacss/dev';
 
-export const defineConfig = (config: object) =>
-  definePandaConfig({
+export const defineConfig = (config: any = {}) => {
+  const { include: includePath, outdir, ...options } = config;
+
+  return definePandaConfig({
     // Where to look for your css declarations
-    include: ['./node_modules/styled-system/exported/**/*.css.{js,jsx,ts,tsx}'],
+    include: [`./node_modules/${outdir}/exported/**/*.css.{js,jsx,ts,tsx}`],
 
     // Files to exclude
     exclude: [],
 
     // The output directory for your css system
-    outdir: './node_modules/styled-system',
+    outdir: 'styled-system',
 
     // Whether to emit the artifacts to node_modules as a package.
     // Will generate a package.json file that contains exports for each of the generated
     // outdir entrypoints
     emitPackage: true,
 
-    ...config,
+    ...options,
   });
+}
 
 export default defineConfig;
