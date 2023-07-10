ab7acdf6c03907cd0bb7f23929855cc7b5024212
Yumin Chen <yumin@chen.so>
2023-07-10 18:10:54 +0100

    Support config path option in cli

--- src/bin.ts
+++ src/bin.ts
@@ -1,13 +1,27 @@
 import { execSync } from 'child_process';
 
-
 const [, , command, ...options] = process.argv;
 
-console.log("Radipan command:", command, ...options)
+console.log('Radipan:', command, ...options);
+
+const readOptionContent = (
+  options: string[],
+  longCmd: string,
+  shortCmd: string
+) => {
+  const index = options.findIndex(
+    option => option === longCmd || option === shortCmd
+  );
+  return (
+    (index >= 0 && options.length > index + 1 && options[index + 1]) || null
+  );
+};
+const configPath =
+  readOptionContent(options, '--config', '-c') || 'radipan.config.ts';
 
 switch (command) {
   case 'css-extract': {
-    if (options.includes("--watch")) {
+    if (options.includes('--watch')) {
       execSync(
         'CSSGEN=pregen npx tsx --watch --tsconfig "node_modules/radipan/extractor.tsconfig.json" "node_modules/radipan/dist/css-extractor/css-extractor.js"',
         { stdio: 'inherit' }
@@ -22,23 +36,31 @@ switch (command) {
     break;
   }
   case 'cssgen': {
-    if (options.includes("--watch")) {
-      execSync('npx radipan css-extract --watch & npx panda cssgen --watch', { stdio: 'inherit' });
+    if (options.includes('--watch')) {
+      execSync(
+        `npx radipan css-extract --watch & npx panda cssgen --config ${configPath} --watch`,
+        { stdio: 'inherit' }
+      );
       break;
     }
 
     execSync(
-      'npx radipan css-extract & npx panda cssgen',
+      `npx radipan css-extract && npx panda cssgen --config ${configPath}`,
       { stdio: 'inherit' }
     );
     break;
   }
   case 'design': {
-    execSync('npx panda studio', { stdio: 'inherit' });
+    execSync(`npx panda studio --config ${configPath}`, { stdio: 'inherit' });
     break;
   }
   case 'prepare': {
-    execSync('panda codegen & npx radipan cssgen', { stdio: 'inherit' });
+    execSync(
+      `npx panda codegen --config ${configPath} && npx radipan cssgen --config ${configPath}`,
+      {
+        stdio: 'inherit',
+      }
+    );
     break;
   }
 }
