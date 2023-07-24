9e8f7841594401c852d11b83bcfee8f3349df959
Yumin Chen <yumin@chen.so>
2023-07-24 16:43:46 +0100

    Add example radipan-vite-react-tsx

---
+++ examples/radipan-vite-react-tsx/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main
+      css={{
+        width: '100%',
+        height: '100vh',
+        color: { base: 'black', _osDark: 'white' },
+        background: { base: 'white', _osDark: 'black' },
+      }}
+    >
+      <div css={{ fontSize: '2xl', fontWeight: 'bold' }}>Hello Radip🐼n!</div>
+    </main>
+  );
+}
+
+export default App;
