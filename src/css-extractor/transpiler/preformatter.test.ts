e2b47b863ea161c89e089d87e99733f3301ae40f
Yumin Chen <yumin@chen.so>
2023-08-10 09:46:37 +0100

    Fix preformatter & addRadipanIdToRadipanSyntax

--- src/css-extractor/transpiler/preformatter.test.ts
+++ src/css-extractor/transpiler/preformatter.test.ts
@@ -2,12 +2,16 @@ import {
   preformat,
   addRadipanIdToJsx,
   addRadipanIdToHyperScript,
+  addRadipanIdToRadipanSyntax,
 } from "./preformatter";
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
+
+const constantizeRadipanId = (source: string) =>
+  source.replace(/\d{2}-[A-Z0-9]{7}/g, "00-XXXXXXX");
 
 describe("preformatter", () => {
   describe("preformat", () => {
-    test("should add radipanId JSX syntax", () => {
+    it("should add radipanId JSX syntax", () => {
       const input = `
 function App() {
   return (
@@ -26,14 +30,14 @@ function App() {
 
 const divCss = { fontSize: "2xl", fontWeight: "bold" };
 
-export default App;
-`;
-      expect(
-        preformat(input).replace(/\d{2}-[A-Z0-9]{7}/g, "00-XXXXXXX")
-      ).toMatchSnapshot();
+export default App;`;
+
+      const result = preformat(input);
+
+      expect(constantizeRadipanId(result)).toMatchSnapshot();
     });
 
-    test("should add radipanId to a mixture of both JSX syntax and HyperScript syntax", () => {
+    it("should add radipanId to a mixture of both JSX syntax and HyperScript syntax", () => {
       const input = `
 const { render } = require("react");
 
@@ -49,23 +53,23 @@ const Button = ({children}) => {
   return (<button role="button">children</button>);
 };
 
-render(<App />, document.body);
-`;
-      expect(
-        preformat(input).replace(/\d{2}-[A-Z0-9]{7}/g, "00-XXXXXXX")
-      ).toMatchSnapshot();
+render(<App />, document.body);`;
+
+      const result = preformat(input);
+
+      expect(constantizeRadipanId(result)).toMatchSnapshot();
     });
   });
 
   describe("addRadipanIdToJsx", () => {
-    test("should add radipanId to JSX syntax", () => {
+    it("should add radipanId to JSX syntax", () => {
       const input = `< div css = {{ color: "red" }}>Hello</div>`;
       const output =
         /<div css={{(\r?\n?\s)*color: "red",?(\r?\n?\s)*}} radipanId="\d{2}-[A-Z0-9]{7}">Hello<\/div>;?/;
       expect(addRadipanIdToJsx(input)).toMatch(output);
     });
 
-    test("should add radipanId to nested JSX elements", () => {
+    it("should add radipanId to nested JSX elements", () => {
       const input = `<main><div css={{
         color: "red",
         background: "white"
@@ -77,31 +81,93 @@ render(<App />, document.body);
   });
 
   describe("addRadipanIdToHyperScript", () => {
-    test("should add radipanId to HyperScript syntax", () => {
+    it("should add radipanId to HyperScript syntax", () => {
       const input = `h("button", { css: { color: "red" } }, "Click me")`;
       const output =
         /h\("button", {\s*css: {\s*color: "red"\s*},\s*radipanId: "\d{2}-[A-Z0-9]{7}"\s*}, "Click me"\);/;
       expect(addRadipanIdToHyperScript(input)).toMatch(output);
     });
 
-    test("should add radipanId to nested HyperScript elements", () => {
-      const input = `
-const { render } = require("react");
-
-const App = () => {
-  return h("div", [
-    h("h1", { className: "App" }, "Hello, world!"),
-    h("p", "This is a paragraph."),
-    h("button", { onClick: () => alert("Clicked!") }, "Click me"),
-  ]);
-};
-
-render(h(App), document.body);
-`;
-
+    it("should add radipanId to HyperScript syntax with ommited props and children", () => {
+      const input = `h(Badge)`;
       const output =
-        /const {\s*render\s*} = require\("react"\);\s*const App = \(\) => {\s*return h\("div", {\s*radipanId: "\d{2}-[A-Z0-9]{7}"\s*}, \[h\("h1", {\s*className: "App",\s*radipanId: "\d{2}-[A-Z0-9]{7}"\s*}, "Hello, world!"\), h\("p", "This is a paragraph."\), h\("button", {\s*onClick: \(\) => alert\("Clicked!"\),\s*radipanId: "\d{2}-[A-Z0-9]{7}"\s*}, "Click me"\)\]\);\s*};\s*render\(h\(App\), document.body\);?/;
+        /h\(Badge,\s*{(\r?\n?\s)*radipanId: "\d{2}-[A-Z0-9]{7}"(\r?\n?\s)*}\);/;
       expect(addRadipanIdToHyperScript(input)).toMatch(output);
+    });
+
+    it("should add radipanId to nested HyperScript elements", () => {
+      const input = `
+      const { render } = require("react");
+
+      const App = () => {
+        return h("div", [
+          h("h1", { className: "App" }, "Hello, world!"),
+          h("p", "This is a paragraph."),
+          h("button", { onClick: () => alert("Clicked!") }, "Click me"),
+        ]);
+      };
+
+      render(h(App), document.body);
+      `;
+
+      const result = addRadipanIdToHyperScript(input);
+
+      expect(constantizeRadipanId(result)).toMatchSnapshot();
+    });
+  });
+
+  describe("addRadipanIdToRadipanSyntax", () => {
+    it("should add radipanId to a simple Radipan Syntax element", () => {
+      const input = `div.create({ css: { color: "red" } }, "whee!")`;
+
+      const result = addRadipanIdToRadipanSyntax(input);
+
+      expect(constantizeRadipanId(result)).toMatchSnapshot();
+    });
+
+    it("should add radipanId to a Radipan Syntax element with ommited props and children", () => {
+      const input = "Badge.create()";
+      const output =
+        /Badge\.create\({(\r?\n?\s)*radipanId: "\d{2}-[A-Z0-9]{7}"(\r?\n?\s)*}\);?/;
+
+      const result = addRadipanIdToRadipanSyntax(input);
+
+      expect(result).toMatch(output);
+    });
+
+    it("should add radipanId to a Radipan Syntax element with ommited props", () => {
+      const input = 'Badge.create("Text")';
+      const output =
+        /Badge\.create\({(\r?\n?\s)*radipanId: "\d{2}-[A-Z0-9]{7}"(\r?\n?\s)*}, "Text"\);?/;
+
+      const result = addRadipanIdToRadipanSyntax(input);
+
+      expect(result).toMatch(output);
+    });
+
+    it("should add radipanId to a nested Radipan Syntax element", () => {
+      const input = `
+      form.create({ css: { display: "flex" } }, [
+        div.create({ css: { color: "green" } }, "green div"),
+        span.create({ css: { color: "red" } }, "red span"),
+        input.create({ css: { color: "blue" }, placeholder: "blue input" }),
+      ])
+    `;
+
+      const result = addRadipanIdToRadipanSyntax(input);
+
+      expect(constantizeRadipanId(result)).toMatchSnapshot();
+    });
+
+    it("should add radipanId to a Radipan Syntax element with no props", () => {
+      const input = 'div.create("Hello")';
+      const output = `div.create({
+  radipanId: "00-XXXXXXX"
+}, "Hello");`;
+
+      const result = addRadipanIdToRadipanSyntax(input);
+
+      expect(constantizeRadipanId(result)).toBe(output);
     });
   });
 });
--- src/css-extractor/transpiler/preformatter.ts
+++ src/css-extractor/transpiler/preformatter.ts
@@ -16,6 +16,7 @@ const genRandomId = (seqNum: number) =>
     .slice(-idLength)
     .toUpperCase();
 
+// Transforms any JSX syntax declared elements and adds a radipanId prop if it doesn't already have it
 export const addRadipanIdToJsx = (source: string) => {
   // Parse the source code into an AST
   const ast = parse(source, {
@@ -27,7 +28,6 @@ export const addRadipanIdToJsx = (source: string) => {
   traverse(ast, {
     // Visit every JSX element node
     JSXElement(path) {
-      seqId++;
       // Get the opening element of the JSX element
       const openingElement = path.node.openingElement;
       // Check if the opening element already has a radipanId prop
@@ -37,6 +37,7 @@ export const addRadipanIdToJsx = (source: string) => {
       );
       // If not, add a new JSX attribute with a random radipanId value
       if (!hasRadipanId) {
+        seqId++;
         const radipanId = `${seqId}-${genRandomId(seqId)}`;
         const jsxAttribute = t.jsxAttribute(
           t.jsxIdentifier("radipanId"),
@@ -52,8 +53,111 @@ export const addRadipanIdToJsx = (source: string) => {
   return code;
 };
 
-// Define a function that transforms any Phy-compatible HyperScript syntax declared elements and adds a radipanId prop if it doesn't already have it
+// Transforms any Phy-compatible HyperScript syntax declared elements and adds a radipanId prop if it doesn't already have it
 export const addRadipanIdToHyperScript = (source: string) => {
+  // Parse the source code into an AST
+  const ast = parse(source, {
+    sourceType: "module",
+    plugins: ["typescript", "jsx"],
+  });
+
+  // Traverse the AST and transform it
+  traverse(ast, {
+    // Visit every call expression node
+    CallExpression(path) {
+      // Get the node from the path
+      const node = path.node;
+      // Check if the node is a Phy-compatible HyperScript syntax declared element
+      if (
+        t.isIdentifier(node.callee) &&
+        node.callee.name === "h" &&
+        Array.isArray(node.arguments) &&
+        node.arguments.length >= 1
+      ) {
+        seqId++;
+        // Get the first argument of the node, which is the component or tag
+        const firstArg = node.arguments[0];
+        // Get the second argument of the node, which is the props object or the children array
+        const secondArg =
+          (node.arguments.length >= 2 && node.arguments[1]) || undefined;
+        // Check if there are only one argument
+        if (node.arguments.length === 1) {
+          // The props and children are omitted
+          // Create a new object expression with a radipanId property
+          const radipanId = `${seqId}-${genRandomId(seqId)}`;
+          const objectProperty = t.objectProperty(
+            t.identifier("radipanId"),
+            t.stringLiteral(radipanId)
+          );
+          const objectExpression = t.objectExpression([objectProperty]);
+          // Insert the object expression as the second argument of the node
+          node.arguments.push(objectExpression);
+        } else if (secondArg && t.isObjectExpression(secondArg)) {
+          // The second argument is the props object
+          // Check if the props object already has a radipanId property
+          const hasRadipanId = secondArg.properties.some(
+            prop =>
+              t.isObjectProperty(prop) &&
+              t.isIdentifier(prop.key) &&
+              prop.key.name === "radipanId"
+          );
+          // If not, add a new object property with a random radipanId value
+          if (!hasRadipanId) {
+            const radipanId = `${seqId}-${genRandomId(seqId)}`;
+            const objectProperty = t.objectProperty(
+              t.identifier("radipanId"),
+              t.stringLiteral(radipanId)
+            );
+            secondArg.properties.push(objectProperty);
+          }
+        } else if (secondArg && t.isArrayExpression(secondArg)) {
+          // The second argument is the children array
+          // Create a new object expression with a radipanId property
+          const radipanId = `${seqId}-${genRandomId(seqId)}`;
+          const objectProperty = t.objectProperty(
+            t.identifier("radipanId"),
+            t.stringLiteral(radipanId)
+          );
+          const objectExpression = t.objectExpression([objectProperty]);
+          // Insert the object expression as the second argument of the node
+          node.arguments.splice(1, 0, objectExpression);
+        } else if (
+          !secondArg ||
+          (t.isIdentifier(secondArg) && secondArg.name === "undefined")
+        ) {
+          // The second argument is undefined or omitted
+          // Create a new object expression with a radipanId property
+          const radipanId = `${seqId}-${genRandomId(seqId)}`;
+          const objectProperty = t.objectProperty(
+            t.identifier("radipanId"),
+            t.stringLiteral(radipanId)
+          );
+          const objectExpression = t.objectExpression([objectProperty]);
+          // Insert the object expression as the second argument of the node
+          node.arguments.push(objectExpression);
+        } else if (t.isStringLiteral(secondArg)) {
+          // The second argument is a string literal, which means it is a single child element
+          // Create a new object expression with a radipanId property
+          const radipanId = `${seqId}-${genRandomId(seqId)}`;
+          const objectProperty = t.objectProperty(
+            t.identifier("radipanId"),
+            t.stringLiteral(radipanId)
+          );
+          const objectExpression = t.objectExpression([objectProperty]);
+          // Insert the object expression as the second argument of the node and move the original second argument to the third position
+          node.arguments.splice(1, 0, objectExpression);
+        }
+      }
+    },
+  });
+
+  // Generate the transformed code from the AST
+  const { code } = generate(ast);
+  return code;
+};
+
+// Transforms any Radipan Syntax declared element and adds a radipanId prop if it doesn't already have it
+export const addRadipanIdToRadipanSyntax = (source: string) => {
   // Parse the source code into an AST
   const ast = parse(source, {
     sourceType: "module",
@@ -67,31 +171,17 @@ export const addRadipanIdToHyperScript = (source: string) => {
       seqId++;
       // Get the node from the path
       const node = path.node;
-      // Check if the node is a Phy-compatible HyperScript syntax declared element
+      // Check if the node is a Radipan Syntax declared element
       if (
-        t.isIdentifier(node.callee) &&
-        node.callee.name === "h" &&
-        Array.isArray(node.arguments) &&
-        node.arguments.length >= 2
+        t.isMemberExpression(node.callee) &&
+        t.isIdentifier(node.callee.object) &&
+        t.isIdentifier(node.callee.property) &&
+        node.callee.property.name === "create" &&
+        Array.isArray(node.arguments)
       ) {
-        // Get the second argument of the node, which is the props object or the children array
-        const secondArg = node.arguments[1];
-        // Check if the second argument is an object expression
-        if (t.isObjectExpression(secondArg)) {
-          // The second argument is the props object
-          // Check if the props object already has a radipanId property
-          const hasRadipanId = secondArg.hasOwnProperty("radipanId");
-          // If not, add a new object property with a random radipanId value
-          if (!hasRadipanId) {
-            const radipanId = `${seqId}-${genRandomId(seqId)}`;
-            const objectProperty = t.objectProperty(
-              t.identifier("radipanId"),
-              t.stringLiteral(radipanId)
-            );
-            secondArg.properties.push(objectProperty);
-          }
-        } else if (t.isArrayExpression(secondArg)) {
-          // The second argument is the children array
+        // Check if there are no arguments
+        if (node.arguments.length === 0) {
+          // The props and children are omitted
           // Create a new object expression with a radipanId property
           const radipanId = `${seqId}-${genRandomId(seqId)}`;
           const objectProperty = t.objectProperty(
@@ -99,8 +189,69 @@ export const addRadipanIdToHyperScript = (source: string) => {
             t.stringLiteral(radipanId)
           );
           const objectExpression = t.objectExpression([objectProperty]);
-          // Insert the object expression as the second argument of the node
-          node.arguments.splice(1, 0, objectExpression);
+          // Insert the object expression as the first argument of the node
+          node.arguments.push(objectExpression);
+        } else {
+          // Get the first argument of the node, which is the props object or the children argument
+          const firstArg = node.arguments[0];
+          // Get the second argument of the node, which is the children array
+          const secondArg = node.arguments[1];
+          // Check if there are only one argument and it is not an object expression
+          if (node.arguments.length === 1 && !t.isObjectExpression(firstArg)) {
+            // The props are omitted, and the first argument is the children argument
+            // Create a new object expression with a radipanId property
+            const radipanId = `${seqId}-${genRandomId(seqId)}`;
+            const objectProperty = t.objectProperty(
+              t.identifier("radipanId"),
+              t.stringLiteral(radipanId)
+            );
+            const objectExpression = t.objectExpression([objectProperty]);
+            // Insert the object expression as the first argument of the node and move the original first argument to the second position
+            node.arguments.unshift(objectExpression);
+          } else if (t.isObjectExpression(firstArg)) {
+            // The first argument is the props object
+            // Check if there are two arguments and the second argument is an array expression or a string literal
+            if (
+              node.arguments.length === 2 &&
+              (t.isArrayExpression(secondArg) || t.isStringLiteral(secondArg))
+            ) {
+              // The second argument is the children array or a single child element
+              // Check if the props object already has a radipanId property
+              const hasRadipanId = firstArg.properties.some(
+                prop =>
+                  t.isObjectProperty(prop) &&
+                  t.isIdentifier(prop.key) &&
+                  prop.key.name === "radipanId"
+              );
+              // If not, add a new object property with a random radipanId value
+              if (!hasRadipanId) {
+                const radipanId = `${seqId}-${genRandomId(seqId)}`;
+                const objectProperty = t.objectProperty(
+                  t.identifier("radipanId"),
+                  t.stringLiteral(radipanId)
+                );
+                firstArg.properties.push(objectProperty);
+              }
+            } else {
+              // The children are omitted, and only the props object is provided
+              // Check if the props object already has a radipanId property
+              const hasRadipanId = firstArg.properties.some(
+                prop =>
+                  t.isObjectProperty(prop) &&
+                  t.isIdentifier(prop.key) &&
+                  prop.key.name === "radipanId"
+              );
+              // If not, add a new object property with a random radipanId value
+              if (!hasRadipanId) {
+                const radipanId = `${seqId}-${genRandomId(seqId)}`;
+                const objectProperty = t.objectProperty(
+                  t.identifier("radipanId"),
+                  t.stringLiteral(radipanId)
+                );
+                firstArg.properties.push(objectProperty);
+              }
+            }
+          }
         }
       }
     },
@@ -112,5 +263,7 @@ export const addRadipanIdToHyperScript = (source: string) => {
 };
 
 export const preformat = (code: string) => {
-  return addRadipanIdToHyperScript(addRadipanIdToJsx(code));
+  return addRadipanIdToRadipanSyntax(
+    addRadipanIdToHyperScript(addRadipanIdToJsx(code))
+  );
 };
